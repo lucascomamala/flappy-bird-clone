07850d6bf988139110da96188060d65f7741abcb
Lucas Comamala <lucascomamala@gmail.com>
2022-08-17 20:07:18 -0600

    Recycle pipes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-
-import Phaser from 'phaser';
+import Phaser from "phaser";
 
 const config = {
   // WebGL
   type: Phaser.AUTO,
-  width: 800,
+  width: 2800,
   height: 600,
   physics: {
     // Arcade physics plugin
-    default: 'arcade',
+    default: "arcade",
     arcade: {
       debug: true,
-    }
+    },
   },
   scene: {
     preload,
@@ -22,7 +21,7 @@ const config = {
 
 const VELOCITY = 200;
 const flapVelocity = 300;
-const initialBirdPos = { x: config.width * 0.1, y: config.height * 0.5 }
+const initialBirdPos = { x: config.width * 0.1, y: config.height * 0.5 };
 
 const PIPES = 4;
 const pipeGapRange = [120, 250];
@@ -34,28 +33,30 @@ let bird = null;
 let pipes = null;
 
 function preload() {
-  this.load.image('sky', 'assets/sky.png');
-  this.load.image('bird', 'assets/bird.png');
-  this.load.image('pipe', 'assets/pipe.png');
+  this.load.image("sky", "assets/sky.png");
+  this.load.image("bird", "assets/bird.png");
+  this.load.image("pipe", "assets/pipe.png");
 }
 
 function create() {
-  this.add.image(0, 0, 'sky').setOrigin(0, 0);
-  bird = this.physics.add.sprite(initialBirdPos.x, initialBirdPos.y, 'bird').setOrigin(0, 0);
+  this.add.image(0, 0, "sky").setOrigin(0, 0);
+  bird = this.physics.add
+    .sprite(initialBirdPos.x, initialBirdPos.y, "bird")
+    .setOrigin(0, 0);
   bird.body.gravity.y = 400;
 
   pipes = this.physics.add.group();
 
   for (let i = 1; i <= PIPES; i += 1) {
-    const upperPipe = pipes.create(0, 0, 'pipe').setOrigin(0, 1);
-    const lowerPipe = pipes.create(0, 0, 'pipe').setOrigin(0, 0);
+    const upperPipe = pipes.create(0, 0, "pipe").setOrigin(0, 1);
+    const lowerPipe = pipes.create(0, 0, "pipe").setOrigin(0, 0);
     placePipe(upperPipe, lowerPipe);
   }
 
   pipes.setVelocityX(-200);
 
-  this.input.on('pointerdown', flap);
-  this.input.keyboard.on('keydown_SPACE', flap);
+  this.input.on("pointerdown", flap);
+  this.input.keyboard.on("keydown_SPACE", flap);
 }
 
 // if bird position y position is smaller than 0 or greater than h of canvas then alert u lost
@@ -63,6 +64,7 @@ function update(time, delta) {
   if (bird.y > config.height || bird.y < -bird.height) {
     restartBirdPosition();
   }
+  recyclePipe();
 }
 
 function placePipe(uPipe, lPipe) {
@@ -78,11 +80,21 @@ function placePipe(uPipe, lPipe) {
   lPipe.y = uPipe.y + pipeGap;
 }
 
+function recyclePipe() {
+  const tempPipes = [];
+  pipes.getChildren().forEach((pipe) => {
+    if (pipe.getBounds().right <= 0) {
+      tempPipes.push(pipe);
+      if(tempPipes.length === 2) {
+        placePipe(...tempPipes);
+      }
+    }
+  });
+}
+
 function getRightMostPipe() {
   let rightMost = 0;
-  debugger
   pipes.getChildren().forEach((pipe) => {
-    debugger
     rightMost = Math.max(pipe.x, rightMost);
   });
   return rightMost;
