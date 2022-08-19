a24d6d2f329472e265a1cdef0083ac41209f5998
Lucas Comamala <lucascomamala@gmail.com>
2022-08-19 10:36:58 -0600

    Pausing the game

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -92,7 +92,16 @@ class PlayScene extends Phaser.Scene {
   }
 
   createPause() {
-    this.add.image(this.config.width - 10, this.config.height - 10, "pause").setScale(3).setOrigin(1, 1);
+    const pauseButton = this.add
+      .image(this.config.width - 10, this.config.height - 10, "pause")
+      .setInteractive()
+      .setScale(3)
+      .setOrigin(1, 1);
+
+    pauseButton.on('pointerdown', () => {
+      this.physics.pause();
+      this.scene.pause();
+    })
   }
 
   handleInputs() {
